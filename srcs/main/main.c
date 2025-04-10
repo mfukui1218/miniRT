e20898ff343650f451ff2e7c9a6e77bc5d1602cf
mfukui1218 <92801943+mfukui1218@users.noreply.github.com>
2025-04-10 15:17:07 +0900

    re

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -4,10 +4,11 @@ int main()
 {
     t_miniRT *miniRT;
 
-    miniRT = malloc(sizeof(t_miniRT));
+    miniRT = malloc(sizeof(t_miniRT *));
     if (!miniRT)
         return (EXIT_FAILURE);
     init_miniRT(miniRT);
+
     return 0;
 }
 
