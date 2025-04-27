9d25771f84173fbe4b2083e9cc0353fc1b4d277d
Michihiro Fukui <mfukui@c4r3s13.42tokyo.jp>
2025-04-27 16:12:39 +0900

    re

--- srcs/ray/hit/hit_cylinder_caps.c
+++ srcs/ray/hit/hit_cylinder_caps.c
@@ -1,53 +1,81 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hit_cylinder_caps.c                                :+:      :+:    :+:   */
+/*   hit_cylinder_body.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
+/*   By: mfukui <mfukui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/15 22:03:31 by mfukui            #+#    #+#             */
-/*   Updated: 2025/04/22 17:11:49 by tookuyam         ###   ########.fr       */
+/*   Created: 2025/04/22 16:28:02 by mfukui            #+#    #+#             */
+/*   Updated: 2025/04/26 22:38:07 by mfukui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static	bool	set_cylinder_quadratic(t_quadratic *q,
-	t_vector dir, t_vector oc, t_cylinder *cylinder)
+static bool	hit_cylinder_cap_top(t_ray ray, t_cylinder *cy, double *t)
 {
-	t_vector	d_proj;
-	t_vector	oc_proj;
-	double		radius;
 	t_vector	axis;
+	t_vector	center;
+	double		denom;
+	double		temp_t;
+	t_vector	hit_point;
 
-	radius = cylinder->radius;
-	axis = vec_normalize(cylinder->orientation);
-	d_proj = vec_sub(dir, vec_scale(axis, vec_dot(dir, axis)));
-	oc_proj = vec_sub(oc, vec_scale(axis, vec_dot(oc, axis)));
-	q->a = vec_dot(d_proj, d_proj);
-	q->b = 2.0f * vec_dot(d_proj, oc_proj);
-	q->c = vec_dot(oc_proj, oc_proj) - radius * radius;
-	q->discriminant = q->b * q->b - 4.0f * q->a * q->c;
-	return (q->discriminant >= 0);
+	axis = vec_normalize(cy->orientation);
+	center = vec_add(cy->position, vec_scale(axis, cy->height));
+	denom = vec_dot(ray.direction, axis);
+	if (fabs(denom) < 1e-6f)
+		return (false);
+	temp_t = vec_dot(vec_sub(center, ray.origin), axis) / denom;
+	if (temp_t < 0.001f)
+		return (false);
+	hit_point = vec_add(ray.origin, vec_scale(ray.direction, temp_t));
+	if (vec_len(vec_sub(hit_point, center)) > cy->radius)
+		return (false);
+	*t = temp_t;
+	return (true);
 }
 
-bool	hit_cylinder_body(t_ray ray, t_cylinder *cylinder, double *t)
+static bool	hit_cylinder_cap_bottom(t_ray ray, t_cylinder *cy, double *t)
 {
-	t_vector	oc;
-	t_quadratic	quadratic;
-	double		h;
-	t_vector	hit;
+	t_vector	axis;
+	t_vector	center;
+	double		denom;
+	t_vector	hit_point;
+	double		temp_t;
 
-	oc = vec_sub(ray.origin, cylinder->position);
-	if (!set_cylinder_quadratic(&quadratic, ray.direction, oc, cylinder))
+	axis = vec_normalize(cy->orientation);
+	center = cy->position;
+	denom = vec_dot(ray.direction, axis);
+	if (fabs(denom) < 1e-6f)
 		return (false);
-	*t = solve_quadratic(quadratic.a, quadratic.b, quadratic.discriminant);
-	if (*t < 0.001f)
+	temp_t = vec_dot(vec_sub(center, ray.origin), axis) / denom;
+	if (temp_t < 0.001f)
 		return (false);
-	hit = vec_add(ray.origin, vec_scale(ray.direction, *t));
-	h = vec_dot(vec_sub(hit, cylinder->position),
-			vec_normalize(cylinder->orientation));
-	if (h < -0.001f || h > cylinder->height + 0.001f)
+	hit_point = vec_add(ray.origin,
+			vec_scale(ray.direction, temp_t));
+	if (vec_len(vec_sub(hit_point, center)) > cy->radius + 1e-6f)
 		return (false);
+	*t = temp_t;
 	return (true);
+}
+
+bool	hit_cylinder_caps(t_ray ray, t_cylinder *cy, double *t)
+{
+	double	t_top;
+	double	t_bot;
+	bool	hit;
+
+	hit = false;
+	if (hit_cylinder_cap_top(ray, cy, &t_top))
+	{
+		*t = t_top;
+		hit = true;
+	}
+	if (hit_cylinder_cap_bottom(ray, cy, &t_bot))
+	{
+		if (!hit || t_bot < *t)
+			*t = t_bot;
+		hit = true;
+	}
+	return (hit);
 }
